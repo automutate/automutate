19c591354559a264dd6d12585017bad53ec5ab1b
Josh Goldberg <josh@fullscreenmario.com>
2016-11-24 11:43:51 -0500

    Added redirection settings to local file provider

--- src/fileProviders/localFileProvider.ts
+++ src/fileProviders/localFileProvider.ts
@@ -1,23 +1,46 @@
 import * as fs from "fs";
+import * as path from "path";
 
 import { IFileProvider } from "../fileProvider";
+
+/**
+ * Settings for manipulating local files.
+ */
+export interface ILocalFileSettings {
+    /**
+     * Directory to read files from, if not the cwd.
+     */
+    readFileDirectory?: string;
+
+    /**
+     * Directory to write files to, if not the cwd.
+     */
+    writeFileDirectory?: string;
+}
 
 /**
  * Provides read-write operations on a local file.
  */
 export class LocalFileProvider implements IFileProvider {
     /**
-     * Name of the file.
+     * Name of the file to read from.
      */
-    private readonly fileName: string;
+    private readonly readFileName: string;
+
+    /**
+     * Name of the file to write to.
+     */
+    private readonly writeFileName: string;
 
     /**
      * Initializes a new instance of the LocalFileProvider class.
      * 
      * @param fileName   Name of the file.
+     * @param fileSettings   Settings for manipulating local files.
      */
-    public constructor(fileName: string) {
-        this.fileName = fileName;
+    public constructor(fileName: string, fileSettings: ILocalFileSettings) {
+        this.readFileName = this.replaceFileDirectoryName(fileName, fileSettings.readFileDirectory);
+        this.writeFileName = this.replaceFileDirectoryName(fileName, fileSettings.writeFileDirectory);
     }
 
     /**
@@ -27,7 +50,7 @@ export class LocalFileProvider implements IFileProvider {
      */
     public read(): Promise<string> {
         return new Promise((resolve, reject): void => {
-            fs.readFile(this.fileName, (error: Error, data: Buffer): void => {
+            fs.readFile(this.readFileName, (error: Error, data: Buffer): void => {
                 error ? reject(error) : resolve(data.toString());
             });
         });
@@ -41,10 +64,22 @@ export class LocalFileProvider implements IFileProvider {
      */
     public write(contents: string): Promise<void> {
         return new Promise((resolve, reject): void => {
-            fs.writeFile(this.fileName, contents, (error: Error): void => {
+            fs.writeFile(this.writeFileName, contents, (error: Error): void => {
                 error ? reject(error) : resolve();
             });
         });
     }
 
+    /**
+     * Resolves a file name to a provided directory, if given.
+     * 
+     * @param fileName   File name to resolve.
+     * @param directory   Directory to resolve within, if any.
+     * @returns The file name, resolved.
+     */
+    private replaceFileDirectoryName(fileName: string, directory?: string) {
+        return directory
+            ? path.resolve(directory, path.basename(fileName))
+            : fileName;
+    }
 }
--- src/mutationsAppliers/fileMutationsApplier.ts
+++ src/mutationsAppliers/fileMutationsApplier.ts
@@ -2,10 +2,26 @@ import * as path from "path";
 
 import { ILogger } from "../logger";
 import { FileProviderFactory } from "../fileProviderFactory";
-import { LocalFileProvider } from "../fileProviders/localFileProvider";
+import { IFileProvider } from "../fileProvider";
+import { ILocalFileSettings, LocalFileProvider } from "../fileProviders/localFileProvider";
 import { MutationsApplier } from "../mutationsApplier";
 import { MutatorFactory } from "../mutatorFactory";
 import { MutatorSearcher } from "../mutatorSearcher";
+
+/**
+ * Settings to apply individual waves of file mutations to local files.
+ */
+export interface IFileMutationSettings {
+    /**
+     * Settings for manipulating local files.
+     */
+    files?: ILocalFileSettings;
+
+    /**
+     * Additional directories to search for mutators within.
+     */
+    mutatorDirectories?: string[];
+}
 
 /**
  * Applies individual waves of file mutations to local files.
@@ -15,13 +31,16 @@ export class FileMutationsApplier extends MutationsApplier {
      * Initializes a new instance of the FileMutationsApplier class.
      * 
      * @param logger   Generates output messages for significant operations.
+     * @param fileSettings   Settings for manipulating local files.
      */
-    public constructor(logger: ILogger) {
+    public constructor(logger: ILogger, settings: IFileMutationSettings = {}) {
         super(
-            new FileProviderFactory(fileName => new LocalFileProvider(fileName)),
+            new FileProviderFactory(
+                (fileName: string): IFileProvider => new LocalFileProvider(fileName, settings.files || {})),
             new MutatorFactory(
                 new MutatorSearcher([
-                    path.join(__dirname, "../../lib/mutators")
+                    path.join(__dirname, "../../lib/mutators"),
+                    ...(settings.mutatorDirectories || [])
                 ]),
                 logger));
     }
